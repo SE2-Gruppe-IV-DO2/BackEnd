e280fbbe229061ae2a001e7a487b6a0c277d5c7c
mastasalami <130776746+mastasalami@users.noreply.github.com>
2024-04-11 07:53:16 +0200

    - Anpassungen für Grundaufbau, damit eine einfache TextMessage zurück geschickt wird (anstelle von einem String)

--- src/main/java/at/aau/serg/websocketdemoserver/websocket/broker/WebSocketBrokerController.java
+++ src/main/java/at/aau/serg/websocketdemoserver/websocket/broker/WebSocketBrokerController.java
@@ -4,6 +4,7 @@ import at.aau.serg.websocketdemoserver.gamelogic.LobbyManager;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
@@ -12,18 +13,18 @@ public class WebSocketBrokerController {
     @MessageMapping("/hello")
     @SendTo("/topic/hello-response")
     public String handleHello(String message) {
-        // TODO handle the messages here
         return "echo from broker: " + HtmlUtils.htmlEscape(message);
     }
 
     @MessageMapping("/create_new_lobby")
     @SendTo("/topic/lobby-created")
-    public String createNewLobby(String userID, String userName) throws Exception {
+    public TextMessage createNewLobby(String userID) throws Exception {
         // Create a new lobby and return its ID
+        String userName = "TEST";
         LobbyManager lobbyManager = LobbyManager.getInstance();
         String newlyCreatedLobbyCode = lobbyManager.createLobby();
         lobbyManager.addPlayerToLobby(newlyCreatedLobbyCode, userID, userName);
-        return newlyCreatedLobbyCode;
+        return new TextMessage(newlyCreatedLobbyCode);
     }
 
     @MessageMapping("/join_lobby")
